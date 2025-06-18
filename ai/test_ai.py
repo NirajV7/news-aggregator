f4e1f86f6b364fa28b3e2f5b110c73e8e8af9019
Niraj <nirajv172717@gmail.com>
2025-06-18 13:41:52 +0530

    Backend and Frontend integrated successfully

--- ai/test_ai.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-print("Testing  Ai environment")
-classifier = pipeline("zero-shot-classification")
-result = classifier("Relience Industries merged with Tata Group", 
-                    candidate_labels = ["Technology", "Business", "Politics" ])
-print(f"Classification: {result['labels'][0]} ({result['scores'][0]:.0%} confidence)")
